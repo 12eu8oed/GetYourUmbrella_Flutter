a203ee91e576acd69533a30e856685565a776261
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-04-08 02:02:21 +0900

    end!

--- flutter_test_1/lib/pages/weather_page.dart
+++ flutter_test_1/lib/pages/weather_page.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_test_1/models/weather_model.dart";
 import "package:flutter_test_1/services/weather_serivce.dart";
+import "package:lottie/lottie.dart";
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -33,26 +34,59 @@ class _WeatherPageState extends State<WeatherPage> {
   }
 
   // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) {
+      return 'assets/sunny.json'; // default to sunny 기본값으로 맑음
+    }
+    
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
 
+  }
   // init state
   @override
   void initState() {
     super.initState();
 
-    // fetch weather in startup
+    // fetch weather on startup
+    _fetchWeather();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[200],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // city name
-            Text(_weather?.cityName ?? "loading city.."),
+            Text(_weather?.cityName ?? "loading city..")
 
+            // animation
+            , Lottie.asset(getWeatherAnimation(_weather?.mainCondition))
+            
             // temperature
-            Text('${_weather?.temperature.round()}°C')
+            , Text('${_weather?.temperature.round()}°C')
+
+            // weather condition
+            , Text(_weather?.mainCondition ?? "")
           ],
         )
       )
